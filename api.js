d6c68a5ed56ded1fc9cc3659ff02ef1bf9be1621
FrancoRosa <km115.franco@gmail.com>
2025-02-07 17:55:18 -0500

    First commit

---
+++ api.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const { getOutDatedNotification, getQuote } = require('./js/helpers');
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+});
+
+client.on('qr', (qr) => {
+    console.log('Scan this QR code with WhatsApp:');
+    qrcode.generate(qr, { small: true });
+});
+
+
+// Function to send messages
+const sendMessage = async (to, text) => {
+    try {
+        await client.sendMessage(to, text);
+        console.log(`... message sent to ${to}: ${text}`);
+    } catch (error) {
+        console.error(`... failed to send message to ${to}:`, error);
+    }
+}
+
+
+client.on('ready', () => {
+    console.log("... whatsapp ready!")
+    const groupId = process.env.WS_GROUP
+
+    setInterval(() => {
+        const time = new Date().toLocaleString("sv")
+        if (time.includes(":00:00")) {
+            console.log("... checking dips")
+            getOutDatedNotification().then(res => {
+                if (res) {
+                    sendMessage(groupId, res)
+                }
+            })
+        }
+        if (time.includes("08:15:15")) {
+            getQuote().then(res => {
+                sendMessage(groupId, res)
+            })
+        }
+    }, 1000);
+});
+
+client.initialize();
