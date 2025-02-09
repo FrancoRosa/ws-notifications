5af3ec546c6842c8a119007647ebe02b5b7296c5
FrancoRosa <km115.franco@gmail.com>
2025-02-09 14:27:34 -0500

    Accept group commands

--- api.js
+++ api.js
@@ -1,47 +1,71 @@
 require("dotenv").config();
-const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
-const { getOutDatedNotification, getQuote } = require('./js/helpers');
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const { getOutDatedNotification, getQuote } = require("./js/helpers");
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
+  authStrategy: new LocalAuth(),
 });
 
-client.on('qr', (qr) => {
-    console.log('... scan this QR code with whatsApp:');
-    qrcode.generate(qr, { small: true });
-});
+const groupId = process.env.WS_GROUP;
 
 
 const sendMessage = async (to, text) => {
-    try {
-        await client.sendMessage(to, text);
-        console.log(`... message sent to ${to}: ${text}`);
-    } catch (error) {
-        console.error(`... failed to send message to ${to}:`, error);
+  try {
+    await client.sendMessage(to, text);
+    console.log(`... message sent to ${to}: ${text}`);
+  } catch (error) {
+    console.error(`... failed to send message to ${to}:`, error);
+  }
+};
+
+const statusMsg = () => {
+  getOutDatedNotification().then((res) => {
+    if (res) {
+      sendMessage(groupId, res);
     }
-}
+  });
+};
 
-client.on('ready', () => {
-    console.log("... whatsapp ready!")
-    const groupId = process.env.WS_GROUP
+const quoteMsg = () => {
+  getQuote().then((res) => {
+    sendMessage(groupId, res);
+  });
+};
 
-    setInterval(() => {
-        const time = new Date().toLocaleString("sv")
-        if (time.includes(":00:00")) {
-            console.log("... checking dips")
-            getOutDatedNotification().then(res => {
-                if (res) {
-                    sendMessage(groupId, res)
-                }
-            })
-        }
-        if (time.includes("08:15:15")) {
-            getQuote().then(res => {
-                sendMessage(groupId, res)
-            })
-        }
-    }, 1000);
+client.on("qr", (qr) => {
+  console.log("... scan this QR code with whatsApp:");
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("... whatsapp ready!");
+
+  setInterval(() => {
+    const time = new Date().toLocaleString("sv");
+    if (time.includes(":00:00")) {
+      statusMsg();
+    }
+    if (time.includes("08:15:15")) {
+      quoteMsg();
+    }
+  }, 1000);
+});
+
+client.on("message", (msg) => {
+  const { from, to, body, type } = msg;
+  console.log({from, body })
+  if (from === 'status@broadcast') {
+    console.log(msg)
+  }
+  if (from === groupId) {
+    if (body.toLowerCase().includes("status")) {
+        statusMsg()
+    }
+    if (body.toLowerCase().includes("quote")) {
+        statusMsg()
+    }
+  }
 });
 
 client.initialize();
